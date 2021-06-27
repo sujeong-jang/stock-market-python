f64508dedd33630e6bf22e20c8690c247d88335f
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2021-06-27 12:05:07 +0900

    관심종목 기능 추가

--- flask_stock.py
+++ flask_stock.py
@@ -1,34 +1,36 @@
-
 import json
 import datetime
 import pandas_market_calendars as mcal
-import exchange_calendars as ecals
 # from FTAPP.flask_stock_def  import *
-from stockapp import flask_stock_def as MY
+from RAAPP import flask_stock_def as MY
 
 from flask import Flask, render_template, session, request, redirect, url_for
 import sqlalchemy as sa
 import cx_Oracle
 
-from pykrx import stock
+app = Flask(__name__, static_folder="./static")
 
-app = Flask(__name__)
-oracle_engine = sa.create_engine('oracle://ft:1234@localhost:1522/xe')
 
-# 세션 사용 시 반드시 임의의 security key 생성
+
+#-----------------------------------------------------------
+# auth : 세션 사용 시 반드시 임의의 security key 생성
+#-----------------------------------------------------------
 app.secret_key = 'asdjfkldjslkfj7ewr8qew668'
 
-#--------------------------------------------------------
-# auth
-#--------------------------------------------------------
-@app.route("/auth_register")
-def auth_register():
-    return render_template('auth_register.html')
-
+#-----------------------------------------------------------
+# auth : 회원가입폼
+#-----------------------------------------------------------
 """
 create table customer(useq number primary key, userid varchar2(20), usernm varchar2(20), userpw varchar2(10), email varchar2(40));
 create sequence customer_seq start with 1 increment by 1;
 """
+@app.route("/auth_register")
+def auth_register():
+    return render_template('auth_register.html')
+
+#-----------------------------------------------------------
+# auth : 회원가입폼 처리부
+#-----------------------------------------------------------
 @app.route("/auth_register_proc", methods=['POST'])
 def auth_register_proc():
     userid = request.form['userid']
@@ -36,11 +38,10 @@ def auth_register_proc():
     userpw = request.form['userpw']
     email = request.form['email']
     print(userid, userpw, usernm, email)
-
     with oracle_engine.connect() as conn:
         trans = conn.begin()
         try:
-            sql = "insert into customer(useq, userid, usernm, userpw, email) values (customer_seq.nextval, :2, :3, :4, :5)"
+            sql = "insert into customer(useq, userid, usernm, userpw, email) values (users_seq.nextval, :2, :3, :4, :5)"
             conn.execute(sql, (userid, usernm, userpw, email))
             trans.commit()
         except Exception as e:
@@ -48,21 +49,17 @@ def auth_register_proc():
             print(e)
             return render_template('error.html')
 
-    return render_template('auth_login.html')
-#--------------------------------------------------------
-@app.route("/auth_logout")
-def auth_logout():
-    session['SESS_LOGIN_STATUS'] = False
-    # session.pop('SESS_USERID', None)
-    # session.pop('SESS_USERNM', None)
-    session.clear()
-    # return redirect(url_for(''))
-    return render_template('index.html')
+    return redirect('auth_login.html')
 
+#-----------------------------------------------------------
+# auth : 로그인폼
+#-----------------------------------------------------------
 @app.route("/auth_login")
 def auth_login():
     return render_template('auth_login.html')
-
+#-----------------------------------------------------------
+# auth : 로그인폼 처리부
+#-----------------------------------------------------------
 @app.route("/auth_login_proc", methods=['POST'])
 def auth_login_proc():
     userid = request.form['userid']
@@ -83,65 +80,129 @@ def auth_login_proc():
         except Exception as e:
             print(e)
             return render_template('error.html')
-    return render_template('index.html')
+    # return render_template('index.html')
+    return redirect(url_for('index'))
 
-#--------------------------------------------------------
+# ----------------------------------------------------------
+# auth : 로그아웃
+# ----------------------------------------------------------
+@app.route("/auth_logout")
+def auth_logout():
+    session['SESS_LOGIN_STATUS'] = False
+    # session.pop('SESS_USERID', None)
+    # session.pop('SESS_USERNM', None)
+    session.clear()
+    return redirect(url_for('index'))
+    #return render_template('index.html')
+
+# ----------------------------------------------------------
 @app.route("/kakao")
 def kakao():
     return render_template('kakao.html')
 
-# ----------------------------------------------------------------
-# 코스닥 코스피 코스피200 지수 정보 가져오기
-# [{'name': '코스피', 'close': 3249.63, 'rate': 0.77001953125},
-# {'name': '코스피 200', 'close': 433.06, 'rate': 0.85986328125},
-# {'name': '코스닥', 'close': 997.42, 'rate': 0.97998046875}]
-# ----------------------------------------------------------------
-def 종합주가지수():
-    krkx = ecals.get_calendar('XKRX')  # 한국 증시 달력
+# ----------------------------------------------------------
+# 관심종목 : 사용자별 관심종목 DB에 delete/select/insert
+# ----------------------------------------------------------
+"""
+create table customer_stock(userid varchar2(20), code varchar2(6), regdate date default sysdate);
+insert into customer_stock(userid, code) values('lkh','006345');
+select distinct code, userid from customer_stock where userid='lkh';
+"""
 
-    today = datetime.date.today()
-    while (True):
-        if krkx.is_session(today.strftime("%Y%m%d")) == False:  # 오늘은 개장일인지 확인
-            today = today - datetime.timedelta(1)
-            continue
-        else:
-            break
+oracle_engine = sa.create_engine('oracle://ft:1234@localhost:1522/xe')
 
-    yesterday = today - datetime.timedelta(1)
-    while (True):
-        if krkx.is_session(yesterday.strftime("%Y%m%d")) == False:  # 어제가 개장일인지 확인
-            yesterday = yesterday - datetime.timedelta(1)
-            continue
-        else:
-            break
+@app.route("/rest_mystock_db")
+def rest_stock_insert():
+    # ---- $.ajax 스크립트에서 넘어온 파라미터 정보
+    code = request.args.get('code', type=str, default='')  #ticker코드
+    mode = request.args.get('mode', type=str, default='')  #입력,삭제,조회 모드
 
-    bf_yesterday = today - datetime.timedelta(2)
-    while (True):
-        if krkx.is_session(yesterday.strftime("%Y%m%d")) == False:  # 엊그제가 개장일인지 확인
-            bf_yesterday = bf_yesterday - datetime.timedelta(1)
-            continue
-        else:
-            break
+    # ---- 회원메뉴 : 세션 아이디가 없으면 에러 처리 
+    if mode=="" or code=="" or session['SESS_USERID'] =="":
+        return render_template('error.html')
+    else :
+        result_str = "ok"
+        with oracle_engine.connect() as conn:
+            trans = conn.begin()
+            try:
+                # ---- 관심종목 : 입력
+                if  mode == "insert":
+                    sql = "insert into customer_stock(userid, code) values (:1, :2)"
+                    conn.execute(sql, (session['SESS_USERID'], code))
+                    trans.commit()
 
-    print(today, yesterday, bf_yesterday)
-    today = today.strftime("%Y%m%d")
-    yesterday = yesterday.strftime("%Y%m%d")
-    bf_yesterday = bf_yesterday.strftime("%Y%m%d")
+                # ---- 관심종목 : 삭제
+                elif mode == "delete":
+                    sql = "delete from customer_stock where userid=:1 and code=:2"
+                    conn.execute(sql, (session['SESS_USERID'], code))
+                    trans.commit()
 
+                # ---- 관심종목 : 조회
+                elif mode =="select":
+                    # ---- 1. 왼쪽 : 탭2개 중 [급등주]
+                    today, yesterday, bf_yesterday = MY.get_today_yesterday()
+                    df_top50 = MY.get_krx_top50(yesterday, today)
+                    df_top50 = df_top50[['종목명','종가','등락률','티커']]
+                    # ---- [내 관심종목] 코드 가져오기
+                    sql = "select distinct code, TRUNC(SYSDATE) - TO_DATE(regdate, 'YY/MM/DD') as regday from customer_stock where userid=:1"
+                    rows = conn.execute(sql, (session['SESS_USERID']))
+                    fetch_rows = rows.mappings().all()  # [{'FirstName': 'Gord', 'LastName': 'Thompson'}, {'FirstName': 'Bob', 'LastName': 'Loblaw'}]
+                    mystock_list = []
+                    for row in fetch_rows:
+                        mystock_list.append(row['code'])
+                    # ---- [급등주] 중 [내 관심종목] 종목만 추리기
+                    mystock_df = df_top50[df_top50['티커'].isin(mystock_list)]
+                    mystock_df = mystock_df.to_json(orient="values")
+                    list_mystock = json.loads(mystock_df)
+                    result_str = json.dumps(list_mystock)
+
+                # ---- 관심종목 : 에러발생
+                else:
+                    raise Exception('에러발생')
+
+            except Exception as e:
+                trans.rollback()
+                print(e)
+                return render_template('error.html')
+    return result_str
+
+
+
+# ----------------------------------------------------------
+#  0. 상단   : 롤링 KOSPI200지수 top50개
+#  1. 왼쪽   : 시장요약,  탭2개[내관심종목, 급등주 top50]
+# ----------------------------------------------------------
+def 인덱스데이터():
+    # ---- 증권 업무일자 : 오늘, 어제, 그제
+    today, yesterday, bf_yesterday = MY.get_today_yesterday()
+
+    # ---- 0. 상단   : 롤링 KOSPI200지수 top50개
+    list_kospi200 = MY.get_krx_kospi200(bf_yesterday, today)
+    print(list_kospi200)
+
+    # ---- #  1. 왼쪽 : 시장요약
     idx_total_list = MY.get_idx_total(yesterday, today)
+    # print(idx_total_list)
+
+    # ---- #  1. 왼쪽 : 탭2개 중 [급등주]
     df_top50 = MY.get_krx_top50(yesterday, today)
-    list_kospi50 = MY.get_krx_kospi50(bf_yesterday, today)
+    df_top50 = df_top50[['종목명', '종가', '등락률', '티커']]
+    print(df_top50.head())
+    
+    # # ---- #  2. 중앙 : 캔들 상단 OHLCV 가져오기
+    # df_ohlcv = MY.get_ohlcv(yesterday, today)
 
-    return idx_total_list, df_top50[['종목명','종가','등락률','티커']], list_kospi50
+    return list_kospi200, idx_total_list, df_top50
 
-# # 종합정보, 시장이슈, 재무정보
-# @app.route("/rest_info")
-# def rest_info():
-
-
+# ----------------------------------------------------------
+#  1. 왼쪽   : 탭2개 중 [급등주 top50]를 위한 페이징
+# ----------------------------------------------------------
 @app.route("/rest_top50_paging")
 def rest_top50():
-    page = request.args.get('page', type=int, default=1)  # 페이지
+    # ---- $.ajax 스크립트에서 넘어온 파라미터 정보
+    page = request.args.get('page', type=int, default=1)
+
+    # ---- 페이징을 위한 start_page, end_page 계산
     page_per_count = 10
     if page>1:
         start_page = (page-1) * page_per_count
@@ -150,51 +211,30 @@ def rest_top50():
         start_page = 0
         end_page = page * page_per_count
 
-    krkx = ecals.get_calendar('XKRX')  # 한국 증시 달력
+    # ---- 증권 업무일자 : 오늘, 어제, 그제
+    today, yesterday, bf_yesterday = MY.get_today_yesterday()
 
-    today = datetime.date.today()
-    while (True):
-        if krkx.is_session(today.strftime("%Y%m%d")) == False:  # 오늘은 개장일인지 확인
-            today = today - datetime.timedelta(1)
-            continue
-        else:
-            break
-
-    yesterday = today - datetime.timedelta(1)
-    while (True):
-        if krkx.is_session(yesterday.strftime("%Y%m%d")) == False:  # 어제가 개장일인지 확인
-            yesterday = yesterday - datetime.timedelta(1)
-            continue
-        else:
-            break
-    print(today, yesterday)
-    today = today.strftime("%Y%m%d")
-    yesterday = yesterday.strftime("%Y%m%d")
-
+    # ----  1. 왼쪽   : 탭2개 중 [급등주 top50]
     df_top50 = MY.get_krx_top50(yesterday, today)
     df_top50 = df_top50[['종목명', '종가', '등락률', '티커']]
-    print(df_top50.head())
+    # ---- [급등주 top50] paging 효과
+    df_top50 = df_top50.iloc[start_page:end_page]
 
-
-    df_top50 = df_top50.iloc[start_page:end_page]  # --------------------------------paging 효과
+    # ---- REST 서비스를 위한 JSON 변환
     list_top50 = df_top50.to_json(orient="values")
     list_top50 = json.loads(list_top50)
     list_top50_String = json.dumps(list_top50)
-    print("----------------------list_top50_String-------------------")
-    print(list_top50_String)
     return list_top50_String
 
-# @ app.route("/header_box")
-# def header_box():
-#     code = request.args.get('code', type=str, default='005930')
-#
-#     get_ohlcv(code)
 
-
-@app.route("/rest_info_tap")
-def rest():
-    dict = {}
-    code = request.args.get('code', type=str, default='005930')  # 페이지
+# ----------------------------------------------------------
+#  3. 오른쪽 : 탭3개 [ 종합정보, 시장이슈, 투자정보&차트 ]
+# ----------------------------------------------------------
+@app.route("/rest_tap3")
+def rest_tap3():
+    dict_tab3 = {}
+    # ---- $.ajax 스크립트에서 넘어온 파라미터 정보
+    code = request.args.get('code', type=str, default='005930') # 종목코드
 
     #------------- 우측 탭메뉴1 - 시세종합 --------------
     df_list = MY.naver_craw_시세종합(code)
@@ -207,13 +247,9 @@ def rest():
     html_tab1 += "<hr>"
     for i, col in enumerate(df_list[1][2].values.tolist()):
         html_tab1 += "<font color=green>├ </font><b>" + str(col) + ": </b>" + str(df_list[1][3].values.tolist()[i]) + "<br>"
-    dict["html_tab1"] = html_tab1
+    dict_tab3["html_tab1"] = html_tab1
 
-    # ------------- 우측 탭메뉴2 - 시장이슈 --------------
-    html_tab2 = MY.naver_craw_news(code)
-    dict["html_tab2"] = html_tab2
-
-    # ------------- 우측 탭메뉴3 - 투자 정보 --------------
+    # ------------- 우측 탭메뉴3 - 투자정보 --------------
     html_tab3 = "<hr>"
     for i, col in enumerate(df_list[5][0].values.tolist()):
         html_tab3 += col + " : " + str(df_list[5][1].values.tolist()[i]) + "<br>"
@@ -223,31 +259,66 @@ def rest():
     html_tab3 += "<hr>"
     for i, col in enumerate(df_list[8][0].values.tolist()):
         html_tab3 += "<font color='red'><b>" + col + " : " + str(df_list[8][1].values.tolist()[i]) + "</b></font><br>"
-    dict["html_tab3"] = html_tab3
+    dict_tab3["html_tab3"] = html_tab3
 
-    # ------------- 차트 상단 box - 시세정보 --------------
-    name = stock.get_market_ticker_name(code)
-    close = df_list[1][1][0]
-    chg = df_list[1][1][2]
-    high = df_list[1][3][4]
-    low = df_list[1][3][5]
-    volume = df_list[1][1][3]
-
-    quote_list = [name, close, chg, high, low, volume]
-    dict["header_box"] = quote_list
-
-    return dict
+    # ------------- 우측 탭메뉴3 - 투자정보 차트 ---------
+    dict_chart = rest_tab3_chart(code)
+    dict_tab3["html_tab3_chart"] = dict_chart
 
 
+    # ------------- 우측 탭메뉴2 - 시장이슈 --------------
+    html_tab2 = MY.naver_craw_news(code)
+    dict_tab3["html_tab2"] = html_tab2
+
+    # ------------- 종목코드 --------------
+    dict_tab3["code"] = code
+
+    return dict_tab3
+
+# ----------------------------------------------------------
+#  3. 오른쪽 : 탭3개 중 [투자정보에 보여질 차트 ]
+# ----------------------------------------------------------
+def rest_tab3_chart(code='005930'):
+    html_tab3_chart = ""
+    html_tab3_chart += "<div class='tab-pane fade active show' id='subchart1'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month1/F_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart2'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month3/F_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart3'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month6/F_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart4'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/year1/F_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart5'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month1/I_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart6'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month3/I_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart7'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/month6/I_" + code + ".png' width='100%'></div>";
+    html_tab3_chart += "<div class='tab-pane fade' id='subchart8'><img src='https://ssl.pstatic.net/imgfinance/chart/trader/year1/I_" + code + ".png' width='100%'></div>";
+    return html_tab3_chart
+
+# ----------------------------------------------------------
+#  index.html 첫 화면
+# ----------------------------------------------------------
 @app.route("/")
 def index():
-    idx_total_list, df_top50, list_kospi50 = 종합주가지수()
+    # ---- 0. 상단   : 롤링 KOSPI200지수 top50개
+    # ---- 1. 왼쪽   : 시장요약,  탭2개[내관심종목, 급등주 top50]
+    list_kospi200, idx_total_list, df_top50 = 인덱스데이터()
     df_top50 = df_top50.iloc[:10]  #--------------------------------paging 효과
     list_top50 = df_top50.to_json(orient="values")
-
     list_top50 = json.loads(list_top50)
 
-    return render_template('index.html', KEY_TOTAL_IDX=idx_total_list, KEY_TOP50=list_top50, KEY_KOSPI50=list_kospi50)
+    # ---- 3. 오른쪽 : 탭3개 [ 종합정보, 시장이슈, 투자정보&차트 ]
+    dict_tab3 = rest_tap3()
+
+    # ---- 3. 오른쪽 : 탭3개 중 [투자정보]에 보여질 차트
+    html_tab3_chart =  rest_tab3_chart()
+
+    return render_template('index.html',
+                           KEY_KOSPI200=list_kospi200,          #상단롤링
+                           KEY_TOTAL_IDX=idx_total_list,        #종합지수
+                           KEY_TOP50=list_top50,                #급등주
+                           KEY_TAB3=dict_tab3,                  #오른쪽 : 탭3개 [ 종합정보, 시장이슈, 투자정보]
+                           KEY_TAB3_CHART = html_tab3_chart     #오른쪽 : 탭3개 중 [투자정보]에 보여질 차트
+                           )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
+
+
+
+
--- flask_stock_def.py
+++ flask_stock_def.py
@@ -1,10 +1,7 @@
-
+import datetime
+import exchange_calendars as ecals
 import json
-from flask import Flask, render_template
-# app = Flask(__name__)
-
-def get_data_db() :
-    pass
+from pykrx import stock
 
 
 # String --> JSON
@@ -98,7 +95,7 @@ def xml_to_json():
 
 #---------------------------------------------------------
 # 기업개황자료 & 재무재표
-# ref : https://opendart.fss.or.kr/  
+# ref : https://opendart.fss.or.kr/
 # 인증키 발급 후 아래 패키지 사용 가능
 # pip install OpenDartReader
 #
@@ -121,7 +118,6 @@ def get_dart_재무재표(code='005930', sdate='2020-05-01', edate='2020-12-31',
     print(df.head())
     print(df.info())
 
-    return df
 
 import requests
 from bs4 import BeautifulSoup
@@ -141,11 +137,55 @@ def naver_국내증시() :
         print(response.status_code)
 
 
-from pykrx import stock
-def get_ohlcv(sdate='20210501', edate='20210520', code=''):
-    # df = stock.get_market_ohlcv_by_date(fromdate="20210501", todate="20210520", ticker="005930")
-    # df
-    pass
+
+# ----------------------------------------------------------
+# 증권 업무일자 : 오늘, 어제, 그제
+# ----------------------------------------------------------
+def get_today_yesterday():
+    krkx = ecals.get_calendar('XKRX')  # 한국 증시 달력
+    #---- 오늘 ----
+    today = datetime.date.today()
+    while (True):
+        if krkx.is_session(today.strftime("%Y%m%d")) == False:  # 오늘은 개장일인지 확인
+            today = today - datetime.timedelta(1)
+            continue
+        else:
+            break
+    # ---- 어제 ----
+    yesterday = today - datetime.timedelta(1)
+    while (True):
+        if krkx.is_session(yesterday.strftime("%Y%m%d")) == False:  # 어제가 개장일인지 확인
+            yesterday = yesterday - datetime.timedelta(1)
+            continue
+        else:
+            break
+    # ---- 그제 ----
+    bf_yesterday = today - datetime.timedelta(2)
+    while (True):
+        if krkx.is_session(bf_yesterday.strftime("%Y%m%d")) == False:  # 엊그제가 개장일인지 확인
+            bf_yesterday = bf_yesterday - datetime.timedelta(1)
+            continue
+        else:
+            break
+    # ----  날짜포맷 ----
+    today = today.strftime("%Y%m%d")
+    yesterday = yesterday.strftime("%Y%m%d")
+    bf_yesterday = bf_yesterday.strftime("%Y%m%d")
+    print(today, yesterday, bf_yesterday)
+
+    return today, yesterday, bf_yesterday
+
+
+# ----------------------------------------------------------
+# 종목코드에 해당하는 OHLCV 가져오기
+# ----------------------------------------------------------
+def get_ohlcv(sdate, edate, code='005930'):
+    df = stock.get_market_ohlcv_by_date(fromdate=sdate, todate=edate, ticker="005930")
+    print("----",df.head())
+    return df
+# today, yesterday, bf_yesterday = get_today_yesterday()
+# get_ohlcv(yesterday,today)
+
 
 #-------------------------------------------
 # 오늘날짜 기준 시세정보 종합 가져오기
@@ -192,9 +232,6 @@ def my_ticker_byname(names):
 
 # stocks = my_ticker(['삼성전자', 'SK하이닉스', '현대자동차', 'NAVER'])
 # print(stocks)
-
-
-
 
 #-------------------------------------------
 # 코드에 해당하는 티커목록 가져오기
@@ -251,33 +288,33 @@ def get_krx_top50(sdate, edate):
     print(df.head())
     return df_top50
 
+
+
 #-------------------------------------------
-#  KOSPI50 전일대비 등락율 가져오기
+#  KOSPI50 : 전일대비 등락율
 # ref : https://github.com/sharebook-kr/pykrx
 #-------------------------------------------
-def get_krx_kospi50(sdate, edate):
+def get_krx_kospi200(sdate, edate):
     종목분류들 = stock.get_index_ticker_list()
     list = []
     for 종목분류코드 in 종목분류들:
         # 세부종목 = stock.get_index_portfolio_deposit_file(종목분류코드)
         # 종목분류명 = stock.get_index_ticker_name(종목분류코드)   #코스피200 1028  KOSPI100 1034  KOSPI50 1035
         # print(종목분류코드, 종목분류명, len(세부종목))
-        if 종목분류코드 == '1035': # KOSPI50 분류코드
+        if 종목분류코드 == '1035':  #KOSPI50
             세부종목 = stock.get_index_portfolio_deposit_file(종목분류코드)
             print(세부종목)
             for ticker_code in 세부종목:
                 indexdata = {}
-                ticker_name = stock.get_market_ticker_name(ticker_code) # 종목명 가져오기
+                ticker_name = stock.get_market_ticker_name(ticker_code)
                 indexdata['code'] = ticker_code
                 indexdata['name'] = ticker_name
-
-                # 전일대비 등락율
+                # print(ticker_code + "\t" + ticker_name)
                 df_chg = stock.get_market_ohlcv_by_date(sdate, edate, ticker_code, freq='d', name_display=True)['종가'].pct_change()*100
                 df_chg = df_chg.dropna(axis=0)
                 indexdata['chg'] = df_chg[0]
-
                 list.append(indexdata)
-
+    # print(list)
     return list
 
 
@@ -292,7 +329,7 @@ def 이동평균선(df, day=1):
 # https://finance.naver.com/item/sise.nhn?code=005930
 #---------------------------------------------------------
 import pandas as pd
-def naver_craw_시세종합(code='005930'):
+def naver_craw_시세종합(code):
     print("naver_craw_시세종합===")
     url_tmpl = 'https://finance.naver.com/item/sise.nhn?code=%s'
     url = url_tmpl % (code)
@@ -301,14 +338,12 @@ def naver_craw_시세종합(code='005930'):
         print(i, "--" * 20)
         print(df.head())
         print(df.info())
-
-    # df = tables[1]
     return data_list
+    # df = tables[1]
+    # return df
 
 # dict = json.loads(res)
     # print(dict)
-
-# 관련 뉴스 크롤링
 def naver_craw_news(code):
     url_tmpl = 'https://finance.naver.com/item/news_news.nhn?code=%s'
     url = url_tmpl % (code)
@@ -323,21 +358,31 @@ def naver_craw_news(code):
         for td in trs:
 
             html_tab2 += "<tr><td>"+td.select_one('td.date').text+"</td>"
-            html_tab2 += "<td><a href='https://finance.naver.com"+td.select_one('a.tit').attrs['href'] +"'>"+td.select_one('a.tit').text+"</a></td>"
+            html_tab2 += "<td><a href='https://finance.naver.com"+td.select_one('a.tit').attrs['href'] +"' target='_top'>"+td.select_one('a.tit').text+"</a></td>"
             html_tab2 += "</tr>"
         print(html_tab2)
     else:
         print(response.status_code)
     return html_tab2
 
+def naver_craw_invest(code):
+    print("=============================")
+    # url_tmpl = 'https://finance.naver.com/item/frgn.nhn?code=%s'
+    # url = url_tmpl % (code)
+    # print(url)
+    html_tab3 = ""
+    html_tab3 += "<b>매매동향 외국인</b>"
+
+    html_tab3 += "<b>매매동향 외국인</b>"
+
+    return html_tab3
+
+
 def naver_craw(code='005930'):
-    url_tmpl = 'https://finance.naver.com/item/main.nhn?code=%s'
+    url_tmpl = 'https://finance.naver.com/item/frgn.nhn?code=%s'
     url = url_tmpl % (code)
-    tables = pd.read_html(url, encoding='euc-kr')
-    print(tables)
-    df = tables[2]
-    print(df)
-    return df
+    response = requests.get(url)
+    print(response.text)
 # df = naver_craw('005930')
 # print(df)
 
